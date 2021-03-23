a398a5313c80c490824c8a78bfd6ffada509ce89
mcquam <murray.mcquarrie@gmail.com>
2021-03-23 16:34:29 +0000

    added dateFns clock format

--- clock.js
+++ clock.js
@@ -3,15 +3,10 @@ const clock = document.querySelector('.clock');
 const tick = () => {
 
     const now = new Date();
-
-    const h = now.getHours();
-    const m = now.getMinutes();
-    const s = now.getSeconds();
+    const newDate = dateFns.format(now, 'HH : mm : ss');
 
     const html = `
-        <span>${h}</span> :
-        <span>${m}</span> :
-        <span>${s}</span>
+        <span>${newDate}</span>
     `;
 
     clock.innerHTML = html;
